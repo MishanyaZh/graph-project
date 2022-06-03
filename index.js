5372514f62081819d0cb9f5e90c1a3a1f32bd74c
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-03 14:14:07 +0200

    add data

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = `
-type Query {
+const typeDefs = gql`
+  type Query {
     greeting: String
-}
+    urls: [String]
+  }
 `;
-const server = new ApolloServer({ typeDefs });
+const data = {
+  greeting: "Hello Misha!",
+  urls: ["https://github.com/MishanyaZh"],
+};
+const server = new ApolloServer({ typeDefs, rootValue: data });
 
 server.listen({ port: 4000 }).then((result) => console.log(result.url));
+console.log("HELLO");
