7b43fca01b6eb84de03d2f74b3c1ec925577b40a
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-03 16:13:08 +0200

    playground and introspection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { ApolloServer, gql } = require("apollo-server");
 const PORT = process.env.PORT || 4000;
 
+// const fs = require("fs");
+// const path = require("path");
+
 const typeDefs = gql`
   type Query {
     greeting: String
@@ -11,7 +14,19 @@ const data = {
   greeting: "Hello Misha!",
   urls: ["https://github.com/MishanyaZh"],
 };
-const server = new ApolloServer({ typeDefs, rootValue: data });
+const server = new ApolloServer({
+  typeDefs,
+  rootValue: data,
+  playground: true,
+  introspection: true,
+});
 
 server.listen({ port: PORT }).then((result) => console.log(result.url));
 console.log("HELLO");
+
+// const server = new ApolloServer({
+//   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+//   resolvers,
+// });
+
+// server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
