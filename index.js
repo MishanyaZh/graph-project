262f6db31988de015a280e58c20cce1b03c78d6d
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-05 10:21:06 +0200

    Dokumentations and description Query

--- index.js
+++ index.js
@@ -9,19 +9,34 @@ const typeDefs = gql`
   schema {
     query: MyQuery
   }
+  """
+  # The object represeting a MyQuery:
+  ## It contains a description MyQuery
+  """
   type MyQuery {
+    "some description to greeting"
     greeting: String!
     greetingCat: String
+    "array strings"
     urls: [String!]!
     randomDiceThrow: Int!
+    "3,14"
     pi: Float!
+    "true, false"
     isTodaySat: Boolean!
+    "true, false array"
     randomCointTossesUntilTrue: [Boolean!]!
     today: DayOfWeek!
+    "workDays description array"
     workDays: [DayOfWeek!]!
+    "randomQuote"
     randomQuote: Quote!
   }
 
+  """
+  # The object represeting a quotes:
+  ## It contains a text and author's name
+  """
   type Quote {
     text: String!
     author: String!
