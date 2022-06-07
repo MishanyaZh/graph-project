fba9c885b2eab038bb8c2c5e70ddca6a33f4f971
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-07 10:36:04 +0200

    add db

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const rootValue = require("./rootValue");
+const resolvers = require("./resolvers");
 const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
   typeDefs,
   rootValue,
+  resolvers,
   playground: true,
   introspection: true,
 });
--- typeDefs.js
+++ typeDefs.js
@@ -1,11 +1,9 @@
 const { gql } = require("apollo-server");
-
 const typeDefs = gql`
   schema {
-    query: MyQuery
+    query: Query
   }
-
-  type MyQuery {
+  type Query {
     authors: [Author!]!
     books: [Book!]!
     users: [User!]!
@@ -29,51 +27,6 @@ const typeDefs = gql`
   type Avatar {
     image: Image!
     color: String!
-  }
-
-  """
-  # The object represeting a MyQuery:
-  ## It contains a description MyQuery
-  """
-  type MyQuery {
-    "some description to greeting"
-    greeting: String!
-    greetingCat: String
-    "array strings"
-    urls: [String!]!
-    randomDiceThrow: Int!
-    "3,14"
-    pi: Float!
-    "true, false"
-    isTodaySat: Boolean!
-    "true, false array"
-    randomCointTossesUntilTrue: [Boolean!]!
-    today: DayOfWeek!
-    "workDays description array"
-    workDays: [DayOfWeek!]!
-    "randomQuote"
-    randomQuoteTheDay: Quote!
-    "randomMovieQuote"
-    randomMovieQuote: String!
-  }
-
-  """
-  # The object represeting a quotes:
-  ## It contains a text and author's name
-  """
-  type Quote {
-    text: String!
-    author: String!
-  }
-
-  enum DayOfWeek {
-    MON
-    TUE
-    WED
-    THU
-    FRI
-    SAT
-    SUN
   }
 `;
 
