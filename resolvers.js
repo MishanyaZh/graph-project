1f2603ce3233fd7b00e7c2931d646de98e8a2448
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-07 10:50:42 +0200

    add resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,25 @@
-const resolvers = {};
+const db = require("./db");
+
+const resolvers = {
+  Book: {
+    title: (parent) => parent.title.toUpperCase(),
+    author: (parent) => db.getAuthorById(parent.authorId),
+  },
+  Author: {
+    books: (parent) => parent.bookIds.map(db.getBookById),
+  },
+  User: {
+    email: (parent) => {
+      console.log("Someone asks about an email.");
+      return parent.email;
+    },
+  },
+  Query: {
+    users: (parent) => {
+      console.log("Query's parent", parent);
+      return parent.users;
+    },
+  },
+};
 
 module.exports = resolvers;
