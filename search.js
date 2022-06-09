a558d55074eb2e9d54694a1370f1e4557e07c9a2
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-09 09:47:59 +0200

    add lib "lunr", refact search

--- search.js
+++ search.js
@@ -1,10 +1,26 @@
+const lunr = require("lunr");
+
+function initBookIndex(db) {
+  return lunr(function () {
+    this.ref("id");
+    this.field("title");
+    this.field("description");
+
+    db.getAllBooks().forEach(function (book) {
+      this.add(book);
+    }, this);
+  });
+}
+
 class Search {
   constructor(db) {
     this.db = db;
+    this.bookIndex = initBookIndex(this.db);
   }
 
   findBooks(searchQuery) {
-    const foundIds = ["1", "12", "20"];
+    const results = this.bookIndex.search(searchQuery);
+    const foundIds = results.map((result) => result.ref);
     return foundIds.map(this.db.getBookById);
   }
 }
