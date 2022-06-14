aae366409a7fb795da054d42ae41a7ba9b527129
Mishanya Zholondkovskyi <76045588+MishanyaZh@users.noreply.github.com>
2022-06-14 20:08:46 +0200

    add findAuthors(), findUsers()

--- search.js
+++ search.js
@@ -1,13 +1,34 @@
 const lunr = require("lunr");
 
-function initBookIndex(db) {
-  return lunr(function () {
+function initBooksIndex(db) {
+  return lunr(function() {
     this.ref("id");
-    this.field("title");
+    this.field("title", { boost: 10 });
     this.field("description");
-
-    db.getAllBooks().forEach(function (book) {
+    db.getAllBooks().forEach(function(book) {
       this.add(book);
+    }, this);
+  });
+}
+
+function initAuthorsIndex(db) {
+  return lunr(function() {
+    this.ref("id");
+    this.field("name", { boost: 10 });
+    this.field("bio");
+    db.getAllAuthors().forEach(function(author) {
+      this.add(author);
+    }, this);
+  });
+}
+
+function initUsersIndex(db) {
+  return lunr(function() {
+    this.ref("id");
+    this.field("name", { boost: 10 });
+    this.field("info");
+    db.getAllUsers().forEach(function(user) {
+      this.add(user);
     }, this);
   });
 }
@@ -15,16 +36,34 @@ function initBookIndex(db) {
 class Search {
   constructor(db) {
     this.db = db;
-    this.bookIndex = initBookIndex(this.db);
+    this.booksIndex = initBooksIndex(this.db);
+    this.usersIndex = initUsersIndex(this.db);
+    this.authorsIndex = initAuthorsIndex(this.db);
   }
 
   findBooks(searchQuery) {
-    const results = this.bookIndex.search(searchQuery);
-    const foundIds = results.map((result) => result.ref);
-    return foundIds.map(this.db.getBookById);
+    const results = [];
+    this.booksIndex
+      .search(searchQuery)
+      .forEach(result => results.push(this.db.getBookById(result.ref)));
+    return results;
+  }
+  findAuthors(searchQuery) {
+    const results = [];
+    this.authorsIndex
+      .search(searchQuery)
+      .forEach(result => results.push(this.db.getAuthorById(result.ref)));
+    return results;
+  }
+  findUsers(searchQuery) {
+    const results = [];
+    this.usersIndex
+      .search(searchQuery)
+      .forEach(result => results.push(this.db.getUserById(result.ref)));
+    return results;
   }
 }
 
 module.exports = {
-  Search,
+  Search
 };
